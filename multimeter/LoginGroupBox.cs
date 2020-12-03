f5ebc09a299bf4bd0c31c23506a9543f38138196
efran <efran@DESKTOP-AOED33A>
2020-12-03 22:21:17 +0800

    test gitee

---
+++ multimeter/LoginGroupBox.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multimeter {
+    public partial class SetupTest {
+        private void login_Click(object sender, EventArgs e) {
+            string userName = this.usernameTBox.Text;
+            string userPassword = this.userpasswordTBox.Text;
+            if (comboBox.SelectedItem.ToString() != "普通用户" && comboBox.SelectedItem.ToString() != "高级用户") {
+                MessageBox.Show("请输入用户类型！", "警告", MessageBoxButtons.OK);
+                return;
+            }
+            if (comboBox.SelectedItem.ToString() == "普通用户") {
+                TestChooseFormShow_Click(sender, e);
+                AdvancedSetting.Visible = false;
+                AdvancedSettingLabel.Visible = false;
+                LoginGroupBox.Visible = false;
+            }//普通用户登录操作
+            else if (userName.Equals("admin") && userPassword.Equals("admin") && comboBox.SelectedItem.ToString() == "高级用户"){
+                TestChooseFormShow_Click(sender, e);
+                TextBoxEnable();
+                AdvancedSetting.Visible = true;
+                AdvancedSettingLabel.Visible = true;
+                LoginGroupBox.Visible = false;
+            }//高级用户登录操作
+
+            else {
+                MessageBox.Show("用户密码或密码错误！", "警告", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void cancel_Click(object sender, EventArgs e) {
+            Close();
+            Application.Exit();
+        }
+        private void userpasswordTBox_TextChanged(object sender, EventArgs e) {
+            userpasswordTBox.PasswordChar = '*';
+        }
+    }
+}
---
+++ multimeter/TestChooseGroupBox.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multimeter
+{
+    public partial class SetupTest {
+        private void testchoose1_Click(object sender, EventArgs e)
+        {
+            SetupTest setupTest = (SetupTest)this.Owner;
+            setupTest.Enabled = true;
+            setupTest.button1_Click(sender, e);
+            Close();
+        }
+
+        private void testchoose2_Click(object sender, EventArgs e)
+        {
+            SetupTest setupTest = (SetupTest)this.Owner;
+            setupTest.Enabled = true;
+            setupTest.TestChoose2_Click(sender, e);
+            Close();
+        }
+
+        private void testchoose3_Click(object sender, EventArgs e)
+        {
+            SetupTest setupTest = (SetupTest)this.Owner;
+            setupTest.Enabled = true;
+            setupTest.TestChoose3_Click(sender, e);
+            Close();
+        }
+
+        private void testchoose4_Click(object sender, EventArgs e)
+        {
+            SetupTest setupTest = (SetupTest)this.Owner;
+            setupTest.Enabled = true;
+            setupTest.TestChoose4_Click(sender, e);
+            Close();
+        }
+
+        private void TestChoose_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void testchoose1_MouseMove(object sender, MouseEventArgs e)
+        {
+            test1.BackColor = Color.Lime;
+        }
+
+        private void testchoose1_MouseLeave(object sender, EventArgs e)
+        {
+            test1.BackColor = Color.White;
+        }
+
+        private void testchoose2_MouseMove(object sender, MouseEventArgs e)
+        {
+            test2.BackColor = Color.Lime;
+        }
+
+        private void testchoose2_MouseLeave(object sender, EventArgs e)
+        {
+            test2.BackColor = Color.White;
+        }
+
+        private void testchoose3_MouseMove(object sender, MouseEventArgs e)
+        {
+            test3.BackColor = Color.Lime;
+        }
+
+        private void testchoose3_MouseLeave(object sender, EventArgs e)
+        {
+            test3.BackColor = Color.White;
+        }
+
+        private void testchoose4_MouseMove(object sender, MouseEventArgs e)
+        {
+            test4.BackColor = Color.Lime;
+        }
+
+        private void testchoose4_MouseLeave(object sender, EventArgs e)
+        {
+            test4.BackColor = Color.White;
+        }
+    }
+}
