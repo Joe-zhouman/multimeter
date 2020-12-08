39d17fe33b1d37f25e45f455793ee35afdd56580
Joe-zhouman <45998136+Joe-zhouman@users.noreply.github.com>
2020-12-08 14:56:21 +0800

    Refactor : move TextBoxChange to ApplyBtnClick

--- multimeter/ApplyBtnClick.cs
+++ multimeter/ApplyBtnClick.cs
@@ -1,20 +1,7 @@
-﻿using System;
+﻿using DataProcessor;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Forms.DataVisualization.Charting;
-using DataProcessor;
-using System.IO;
-using System.Runtime.CompilerServices;
-using CCWin;
-using CCWin.SkinControl;
-using System.Runtime.InteropServices;
-using System.Threading;
 
 namespace multimeter
 {
@@ -186,7 +173,48 @@ namespace multimeter
 
             return CheckData.CheckDoubleList(positionList);
         }
+        private void HeatMeterToBox(HeatMeter heatMeter, List<TextBox> positionBoxes, List<TextBox> channelBoxes, TextBox diameterBox,
+            TextBox kappaBox)
+        {
+            for (int i = 0; i < heatMeter.TestPoint; i++)
+            {
+                positionBoxes[i].Text = heatMeter.Position[i];
+                channelBoxes[i].Text = heatMeter.Channel[i];
+            }
+            diameterBox.Text = heatMeter.Diameter;
+            kappaBox.Text = heatMeter.Kappa;
+        }
+        private void SampleToBox(Sample sample, List<TextBox> positionBoxes, List<TextBox> channelBoxes, TextBox diameterBox
+        )
+        {
+            for (int i = 0; i < sample.TestPoint; i++)
+            {
+                positionBoxes[i].Text = sample.Position[i];
+                channelBoxes[i].Text = sample.Channel[i];
+            }
+            diameterBox.Text = sample.Diameter;
+        }
+        private void BoxToHeatMeter(ref HeatMeter heatMeter, List<TextBox> positionBoxes, List<TextBox> channelBoxes,
+            TextBox kappaBox)
+        {
+            for (int i = 0; i < heatMeter.TestPoint; i++)
+            {
+                heatMeter.Position[i] = positionBoxes[i].Text;
+                heatMeter.Channel[i] = channelBoxes[i].Text;
+            }
 
+            heatMeter.Kappa = kappaBox.Text;
+        }
+        private void BoxToSample(ref Sample sample, List<TextBox> positionBoxes, List<TextBox> channelBoxes, TextBox diameterBox
+        )
+        {
+            for (int i = 0; i < sample.TestPoint; i++)
+            {
+                sample.Position[i] = positionBoxes[i].Text;
+                sample.Channel[i] = channelBoxes[i].Text;
+            }
+            sample.Diameter = diameterBox.Text;
+        }
         public void TextBoxEnable()
         {
             List<GroupBox> groupBoxs = new List<GroupBox>()
